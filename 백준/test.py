319d48da8904b1d64417273b18cc8b9ee4932730
unknown <jhc03032@naver.com>
2024-10-16 23:37:35 +0900

    로컬 변경 사항 커밋

--- 백준/test.py
+++ 백준/test.py
@@ -1,23 +1,53 @@
 import sys
 sys.stdin = open("C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt")
 
+# SWEAR 13732 정사각형 판정 문제와 비슷함
 
-def dfs(n,s,lst) :
 
-    if n == M :
-        ans.append(lst)
-        return
+# 너비 구하기
+def check_row(sr,sc) :
+    global row
+    for r in range(sr+1,10) :
+        if arr[r][sc] == 1 :
+            row += 1
+        else :
+            break
+    return row
+
+def check_col(sr,sc) : 
+    global col
+    for c in range(sc+1,10) :
+        if arr[sr][c] == 1 :
+            col += 1
+        else :
+            break
+    return col
     
-    if s > N :
-        return
 
-    for j in range(s,N+1) :
-        dfs(n+1,j,lst+[j])
+# 네모 확인
+def check_rec(sr,sc) :
+    for r in range(sr+1,10) :
+        for c in range(sc+1,10) :
+            if arr[r][c] == 0 :
+                
 
-N,M = map(int,input().split())
-ans = []
+arr = [list(map(int,input.split())) for _ in range(10)]
 
-dfs(0,1,[])
 
-for lst in ans :
-    print(*lst)
+
+sr,sc = 10,10       # 좌상단
+er,ec = -1,-1       # 우하단
+for i in range(10) :
+    for j in range(10) :
+        row = 1
+        col = 1
+        if arr[i][j] == 1 :
+            check_row(i,j)
+            check_col(i,j)
+            if row == col :
+                check_rec(i,j)
+            
+
+
+
+        
