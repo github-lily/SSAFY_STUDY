d5ff3eca7586b0c180de711edd9eeeb569893324
unknown <jhc03032@naver.com>
2024-10-20 23:54:50 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,30 +1,24 @@
 import sys
 sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
-2588
 
 
+N,M = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(N)]
+ans_lst = []
+
+for _ in range(M) :
+    sr,sc,er,ec = map(int,input().split())
+    sr,sc,er,ec = sr-1,sc-1,er-1,ec-1       # 인덱스 맞춰주기
+    sum_val = 0                             # 구간합
+
+    for r in range(sr,er+1) :
+        for c in range(sc,ec+1) :
+            sum_val += arr[r][c]
+    ans_lst.append(sum_val)
 
 
-# A = int(input())
-# B = input()
-
-# C = A * int(B[2])
-# D = A * int(B[1])
-# E = A * int(B[0])
-# b = int(B)
-# ans = A * b
+for ans in ans_lst :
+    print(ans)
 
 
-# print(C)
-# print(D)
-# print(E)
-# print(ans)
-
-
-
-a=int(input())
-b=input()
-for i in range(1,4):
-    print(a*int(b[-i]))
-print(a*int(b))
