3dc3c34bb4647be7e95436efabf8b239d5380eea
unknown <jhc03032@naver.com>
2024-10-24 00:00:49 +0900

    1

--- SWEA/D3/test.py
+++ SWEA/D3/test.py
@@ -1,8 +1,38 @@
 import sys
 sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/SWEA/D3/test.txt')
 
+dr = [0,1,0,-1]
+dc = [1,0,-1,0]
+
+def dfs(x,y) :
+    v[x][y] = 1
+
+    for i in range(4) :
+        nr, nc = x+dr[i], y+dc[i]
+        if 0<=nr<N and 0<=nc<M and arr[nr][nc] == 1 and v[nr][nc] == 0 :
+            dfs(nr,nc)
 
 
+T = int(input())
+for _ in range(T) :
+    M, N, K = map(int,input().split())
+
+    # 필요 변수 선언
+    ans = 0
+    arr = [[0] * M for _ in range(N)]
+    v = [[0] * M for _ in range(N)]
+
+    # 배추 심기
+    for _ in range(K) :
+        X,Y = map(int,input().split())
+        arr[Y][X] = 1
 
 
-    
+    # 배추밭 탐색
+    for r in range(N) :
+        for c in range(M) :
+            if arr[r][c] == 1 and v[r][c] == 0 :
+                dfs(r,c)
+                ans += 1
+
+    print(ans)
