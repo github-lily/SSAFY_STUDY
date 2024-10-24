0a24b09c4318b39a9b3eb2b5d06f556aa3be4870
unknown <jhc03032@naver.com>
2024-10-24 23:57:19 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,60 +1,61 @@
 import sys
-sys.stdin = open('C:/Users/SSAFY/Desktop/portfolio/백준/test.txt')
+sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
 
-R,C,M = map(int,input().split())
-shark = []
-ans = 0
-for _ in range(M) :
-    i,j,s,d,z = list(map(int,input().split()))
-    shark.append([i-1,j-1,s,d,z])       # i,j 좌표를 0부터 시작하게 저장
+# dr = []
 
+# T = int(input())
+# for tc in range(1,T+1) :
+#     N = int(input())
+#     arr = [list(map(int,input().split())) for _ in range(N)]
 
-W,H = 2*C-2, 2*R-2      #반복되는 구간 설정
-# 구간 테이블 설정
-wtbl = [n for n in range(C)] + [n for n in range(C-2,0,-1)]
-htbl = [n for n in range(R)] + [n for n in range(R-2,0,-1)]
-opp = {1:2, 2:1, 3:4, 4:3}      # 방향 전환 dict
-
-# 1. j열의 가장 위 상어를 잡기(j열에서 처음 만난 상어)
-#[0] : i, [1] : j, [2] : speed, [3] : 방향, [4] 크기
-for fisher in range(C) : # 0열~C-1열까지 처리
+#     ans = -1
+#     for i in range(N) :
+#         for j in range(N) :
+#             dfs(i,j,[],0)
     
-    # 열, 행 오름차순 정렬
-    shark.sort(key=lambda x : (x[1],x[0]))  
+#     print(ans)
 
-    for i in range(len(shark)) : # 잡으면 사라지니 len으로
-        if shark[i][1] == fisher :
-            ans += shark[i][4]
-            shark.pop(i)
-            break
-                
-    # 2. 상어 이동
-    for i in range(len(shark)) :
-        #좌우방향으로 움직일 경우
-        if shark[i][3] >= 3 :
-            dr = 1 if shark[i][3] == 3 else -1          # 우측이면 1, 좌측이면 -1
-            shark[i][1] = (shark[i][1] + shark[i][2] *dr) % W
-            if C <= shark[i][1] :
-                shark[i][1] = wtbl[shark[i][1]]         # 좌표 변환
-                shark[i][3] = opp[shark[i][3]]          # 방향 반대
 
-        # 상하로 움직일 경우
-        else :
-            dr = 1 if shark[i][3] == 2 else -1          # 아래면 1, 위면 -1
-            shark[i][0] = (shark[i][0] + shark[i][2] *dr) % H
-            if R <= shark[i][0] :
-                shark[i][0] = htbl[shark[i][0]]         # 좌표 변환
-                shark[i][3] = opp[shark[i][3]]          # 방향 반대
 
-    # 3. 상어 정렬 후 아래서부터 작은 상어가 같은 좌표면 삭제
-    # 행과 열은 오름차순, 크기는 내림차순으로 정렬
-    shark.sort(key=lambda x : (x[1],x[0],-x[4])) 
-    # 가장 끝에서 1번까지 처리
-    for i in range(len(shark)-1,0,-1) :
-        # 위의 상어가 나의 좌표와 같다면 나를 삭제 (크기 내림차순이니까)
-        if (shark[i][0:2]) == (shark[i-1][0:2]) :
-            shark.pop(i)
+def dfs(r,c,cnt) :
+    global ans, sr,sc, er, ec
+    
+    if cnt > ans :
+        return
+    
+    if r == er and c == ec :
+        ans = min(ans,cnt)
+        return ans
+    
+    # 오른쪽과 아래만 확인하면 됨(우하)
+    dr = [0,1]
+    dc = [1,0]
+    for i in range(2) :
+        nr, nc = r + dr[i], c+dc[i]
+        # 체스판의 범위 이내여야 함
+        if sr<= nr <= er and sc<= nc <= ec :
+            if arr[r][c] == arr[nr][nc] :   # 같다면
+                cnt += 1
+                if arr[r][c] == 'W' :
+                    arr[nr][nc] = 'B'
+                else :
+                    arr[nr][nc] = 'W'
+    
+    
 
-        
+
+M,N = map(int,input().split())
+arr = [list(input()) for _ in range(M)]
+ans = 99999999999999999999999999999999999999999999999
+
+for i in range(N) :
+    for j in range(M) :
+        # 오른쪽, 아래쪽으로 8x8을 만들 수 있을 때 dfs 호출
+        # 종료를 위해 끝 지점 저장
+        sr, sc = i,j
+        er, ec = i+7, j+7
+        if 0<= er <M and 0<=ec<N :
+            dfs(i,j,0)
+
 print(ans)
