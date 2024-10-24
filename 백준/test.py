4de6e28d065585a9d1b35a65a6a054c6b891c909
이현희 <jhc03032@naver.com>
2024-10-24 17:58:25 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,86 +1,60 @@
 import sys
-sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
+sys.stdin = open('C:/Users/SSAFY/Desktop/portfolio/백준/test.txt')
 
-''' 진행 순서
-1. 낚시꾼의 열 이동
-2. 상어 낚시(제거)
-3. 상어 이동
-4. 겹치는 상어 제거
-'''
 
 R,C,M = map(int,input().split())
-info = [list(map(int,input().split())) for _ in range(M)]
-ans = 0     # 상어 크기의 합
+shark = []
+ans = 0
+for _ in range(M) :
+    i,j,s,d,z = list(map(int,input().split()))
+    shark.append([i-1,j-1,s,d,z])       # i,j 좌표를 0부터 시작하게 저장
 
-# 상어 정보를 담은 배열
-# [0][1] : 상어의 위치, [2] : 속도, [3] : 방향, [4] : 크기
-# 행, 열, 크기 순으로 정렬(오름차순) -> 땅(0번행)에 가깝고 크기가 작은 물고기가 위로가게
-info.sort(key=lambda x : (x[0],x[1],x[4]))
 
-# 이동 방향 리스트(상하우좌)
-dr,dc = [0,-1,1,0,0],[0,0,0,1,-1]
+W,H = 2*C-2, 2*R-2      #반복되는 구간 설정
+# 구간 테이블 설정
+wtbl = [n for n in range(C)] + [n for n in range(C-2,0,-1)]
+htbl = [n for n in range(R)] + [n for n in range(R-2,0,-1)]
+opp = {1:2, 2:1, 3:4, 4:3}      # 방향 전환 dict
 
-# 방향 전환 리스트(상하우좌, 역방향)
-reverse = [0,2,1,4,3]
+# 1. j열의 가장 위 상어를 잡기(j열에서 처음 만난 상어)
+#[0] : i, [1] : j, [2] : speed, [3] : 방향, [4] 크기
+for fisher in range(C) : # 0열~C-1열까지 처리
+    
+    # 열, 행 오름차순 정렬
+    shark.sort(key=lambda x : (x[1],x[0]))  
 
-#낚시꾼의 열 이동, 상어 낚시
-k = 0   #낚시꾼의 위치
-while k < R :
-    for i in range(R) :
-        if info[i][1] == k :        # 낚시꾼과 같은 위치라면
-            ans += info[i][4]       # 상어 크기 더해주기
-            info.pop(i)             # 낚시한 상어 제거
+    for i in range(len(shark)) : # 잡으면 사라지니 len으로
+        if shark[i][1] == fisher :
+            ans += shark[i][4]
+            shark.pop(i)
             break
-    
-    # 상어 이동
-    
-    for n in range(len(info)) :
+                
+    # 2. 상어 이동
+    for i in range(len(shark)) :
+        #좌우방향으로 움직일 경우
+        if shark[i][3] >= 3 :
+            dr = 1 if shark[i][3] == 3 else -1          # 우측이면 1, 좌측이면 -1
+            shark[i][1] = (shark[i][1] + shark[i][2] *dr) % W
+            if C <= shark[i][1] :
+                shark[i][1] = wtbl[shark[i][1]]         # 좌표 변환
+                shark[i][3] = opp[shark[i][3]]          # 방향 반대
 
-
-        # 잘못된 코드1 : 속도가 (R-1)*2 or (C-1)*2 보다 큰 수의 경우 오류가 남
-
-        # # 방향, 속도만큼 행과 열 이동
-        # info[n][0] += dr[info[n][3]] * info[n][2]       # 행
-        # info[n][1] += dc[info[n][3]] * info[n][2]       # 열
-
-        # if info[n][0] < 0 :     # 위쪽으로 벗어남
-        #     info[n][0] += R
-            # info[n][3] = reverse[info[n][3]]    # 방향 전환
-        # if info[n][0] > R-1 :   # 아래쪽으로 벗어남
-        #     info[n][0] -= R
-        #     info[n][3] = reverse[info[n][3]]
-        # if info[n][1] < 0 :     # 왼쪽으로 벗어남
-        #     info[n][1]      # 모르겠음
-        #     info[n][3] = reverse[info[n][3]]
-        # if info[n][1] > C-1 :   # 오른쪽으로 벗어남
-        #     info[n][1]      # 모르겠음
-        #     info[n][3] = reverse[info[n][3]]
-
-        # 잘못된 코드2 : 기존값+이동값이 범위를 벗어날 수 있음
-        # 상하일때
-        # if info[n][3] == 1 or info[n][3] == 2 :
-        #     info[n][0] += (info[n][2]%R)
-
-        # # 좌우일때
-        # else :          # if info[n][3] == 3 or info[n][3] == 4 :
-        #     info[n][1] += (info[n][2]%C)        
-
-
-        # info[n][3] = reverse[info[n][3]]    # 방향 전환
-
-
-
-    
-    # 같은 위치 걸러주기
-    r = 1
-    while r < len(info) :
-        if info[r-1][0:2] == info[r][0:2] :
-            info.pop(r-1)           # 크기가 작은 상어 제거
+        # 상하로 움직일 경우
         else :
-            r+1    
-    
-    # 낚시꾼 위치 이동
-    k += 1 
+            dr = 1 if shark[i][3] == 2 else -1          # 아래면 1, 위면 -1
+            shark[i][0] = (shark[i][0] + shark[i][2] *dr) % H
+            if R <= shark[i][0] :
+                shark[i][0] = htbl[shark[i][0]]         # 좌표 변환
+                shark[i][3] = opp[shark[i][3]]          # 방향 반대
+
+    # 3. 상어 정렬 후 아래서부터 작은 상어가 같은 좌표면 삭제
+    # 행과 열은 오름차순, 크기는 내림차순으로 정렬
+    shark.sort(key=lambda x : (x[1],x[0],-x[4])) 
+    # 가장 끝에서 1번까지 처리
+    for i in range(len(shark)-1,0,-1) :
+        # 위의 상어가 나의 좌표와 같다면 나를 삭제 (크기 내림차순이니까)
+        if (shark[i][0:2]) == (shark[i-1][0:2]) :
+            shark.pop(i)
+
         
-    
 print(ans)
