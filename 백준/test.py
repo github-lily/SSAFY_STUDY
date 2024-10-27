832b7640b7c7060c74bdbe22202628814d9d2349
unknown <jhc03032@naver.com>
2024-10-27 22:32:56 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -4,14 +4,7 @@ sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
 N = int(input())
 arr = list(map(int,input().split()))
-v = int(input())
 
-
-ans = 0
-for num in arr :
-    if num == v :
-        ans += 1
-
-print(ans)
-
-
+min_v = min(arr)
+max_v = max(arr)
+print(f'{min_v} {max_v}')
