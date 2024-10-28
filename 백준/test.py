e1a66ae246a3c8de933d4255249db7372430c689
unknown <jhc03032@naver.com>
2024-10-28 23:31:08 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -2,9 +2,17 @@ import sys
 sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
 
-N = int(input())
-arr = list(map(int,input().split()))
 
-min_v = min(arr)
-max_v = max(arr)
-print(f'{min_v} {max_v}')
+
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  ctx = {}
+
+  for _ in range(n):
+    s, l = input().split()
+
+    ctx[s] = ctx.get(s, 0) + int(l)
+
+  print(max(ctx, key=ctx.get))
