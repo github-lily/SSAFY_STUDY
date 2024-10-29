77260ca37418837087a5bb43f8a967be1439397b
unknown <jhc03032@naver.com>
2024-10-29 21:49:22 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -4,15 +4,10 @@ sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
 
 
-t = int(input())
+a,b = map(int,input().split())
 
-for _ in range(t):
-  n = int(input())
-  ctx = {}
-
-  for _ in range(n):
-    s, l = input().split()
-
-    ctx[s] = ctx.get(s, 0) + int(l)
-
-  print(max(ctx, key=ctx.get))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
