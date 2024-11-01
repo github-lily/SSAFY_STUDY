0830a428a58ac6093cde8aa3f8580c03c6cdc796
이현희 <jhc03032@naver.com>
2024-11-01 15:28:47 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,13 +1,11 @@
 import sys
 sys.stdin = open('C:/Users/SSAFY/Desktop/portfolio/백준/test.txt')
 
-max_v = 0
-max_idx = 0
-for i in range(1,10) :
-    num = int(input())
-    if num > max_v :
-        max_v = num
-        max_idx = i
 
-print(max_v)
-print(max_idx)
+A,B,C = map(int,input().split())
+
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
+       
