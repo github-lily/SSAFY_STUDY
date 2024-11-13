8429b9fc8b23a6256767ec4b47f61ecabbc81557
이현희 <jhc03032@naver.com>
2024-11-13 17:00:23 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,27 +1,35 @@
 import sys
-sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
+sys.stdin = open('C:/Users/SSAFY/Desktop/portfolio/백준/test.txt')
+
+def dfs(n,alst,blst) :
+    global ans
+
+    if n == N :                     #재료를 모두 다 담으면 
+        return  
+    
+    if len(blst) == M :
+        asum=bsum=0                 # 요리의 맛
+        for i in range(M) :         # 재료 리스트의 개수가 M이므로 M까지
+            for j in range(M) :
+                asum += arr[alst[i]][alst[j]]
+                bsum += arr[blst[i]][blst[j]]
+        ans = min(ans,abs(asum-bsum))   
+    
+    # A 재료담기
+    dfs(n+1,alst+[n],blst) 
+    # B 재료담기
+    dfs(n+1,alst,blst+[n]) 
 
 
-def backtracking() :
-    if N == 
-
-
-T = map(int(input()))
+T = int(input())
 for tc in range(1,T+1) :
     N = int(input())
-    # 식재료 배열
-    arr = [list(map(int,input.split())) for _ in range(N)]
+    M = N//2        # 요리 재료의 절반
+    arr = [list(map(int,input().split())) for _ in range(N)]
 
-    # 맛 점수 기록할 배열
-    taste = [[0]*(N) for _ in range(N)]
+    ans = 20000*N*N
 
-    for r in range(N) :
-        for c in range(N) :
-            taste[r][c] = arr[r][c] + arr[c][r]
-            taste[r][c] = taste[c][r]           # 대칭
+    dfs(0,[],[])        #각 요리 재료를 담을 리스트
+        
 
-    # 식재료 사용 표시 배열
-    v = [0]*N
-
-    for r in range(N) :
-        for c in range(N) :
+    print(f'#{tc} {ans}')
