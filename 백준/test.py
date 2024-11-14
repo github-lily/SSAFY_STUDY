6ba47cc877f9a92be3089bfc01a88eab3ae71b68
unknown <jhc03032@naver.com>
2024-11-14 23:12:34 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,35 +1,57 @@
 import sys
-sys.stdin = open('C:/Users/SSAFY/Desktop/portfolio/백준/test.txt')
-
-def dfs(n,alst,blst) :
-    global ans
-
-    if n == N :                     #재료를 모두 다 담으면 
-        return  
-    
-        if len(blst) == M :
-            asum=bsum=0                 # 요리의 맛
-            for i in range(M) :         # 재료 리스트의 개수가 M이므로 M까지
-                for j in range(M) :
-                    asum += arr[alst[i]][alst[j]]
-                    bsum += arr[blst[i]][blst[j]]
-            ans = min(ans,abs(asum-bsum))   
-    
-    # A 재료담기
-    dfs(n+1,alst+[n],blst) 
-    # B 재료담기
-    dfs(n+1,alst,blst+[n]) 
+sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
 
-T = int(input())
-for tc in range(1,T+1) :
+import heapq
+
+INF = int(1e9)
+
+# 우하좌상 방향
+di = [0,1,0,-1]
+dj = [1,0,-1,0]
+
+# 테스트케이스 넘버
+tc = 0
+
+while True :                # 테스트 케이스 개수가 주어지지 않음
+    tc += 1                 # 테스트 케이스 +1
     N = int(input())
-    M = N//2        # 요리 재료의 절반
+    if N == 0 : break       # 0이 입력되면 종료
+
     arr = [list(map(int,input().split())) for _ in range(N)]
 
-    ans = 20000*N*N
+    min_val = [[INF]*N for _ in range(N)]   # 최소 금액을 저장할 배열
 
-    dfs(0,[],[])        #각 요리 재료를 담을 리스트
-        
 
-    print(f'#{tc} {ans}')
+    def dijkstra(si,sj) :
+        q = []
+        min_val[si][sj] = arr[si][sj]             # 주의할 점! : 이 문제는 0,0도 가중치가 있음
+        heapq.heappush(q,(arr[si][sj],si,sj))     # 가중치, 시작점 좌표 # 가중치 값 주의
+
+
+        while q :
+            loss, ci,cj = heapq.heappop(q)
+            
+            # 목적지 도달
+            if ci == N-1 and cj == N-1 : return
+
+
+            # 손실 금액이 최소 손실 금액보다 크다면 pass
+            if loss > min_val[ci][cj] : continue
+            for k in range(4) :
+                ni,nj = ci+di[k], cj+dj[k]
+                if 0<= ni < N and 0 <= nj < N :     # 범위 이내일 때
+                    go_loss = loss + arr[ni][nj]    # ni,nj에 가는데 드는 비용
+                    if go_loss < min_val[ni][nj] :  # 새로운 값이 더 작다면
+                        min_val[ni][nj] = go_loss   # 최소 비용 갱신
+                        heapq.heappush(q,(go_loss,ni,nj))
+
+    dijkstra(0,0)
+    ans = min_val[N-1][N-1]
+    print(f'Problem {tc}: {ans}')
+
+            
+            
+
+
+            
