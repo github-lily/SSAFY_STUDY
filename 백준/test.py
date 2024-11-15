88f7960b21a816d5318360554855cb320925bfe0
이현희 <jhc03032@naver.com>
2024-11-15 10:03:35 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,54 +1,57 @@
 import sys
-sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
+sys.stdin = open('C:/Users/SSAFY/Desktop/portfolio/백준/test.txt')
+
 
 
 import heapq
 
+V,E = map(int,input().split())
+start = int(input())
+
 INF = int(1e9)
+# V개의 정점에서 V개의 정점으로 가는 최단경로 기록 리스트
+min_val = [INF]*(V+1)
 
-# 우하좌상 방향
-di = [0,1,0,-1]
-dj = [1,0,-1,0]
-
-# 테스트케이스 넘버
-tc = 0
-
-while True :                # 테스트 케이스 개수가 주어지지 않음
-    tc += 1                 # 테스트 케이스 +1
-    N = int(input())
-    if N == 0 : break       # 0이 입력되면 종료
-
-    arr = [list(map(int,input().split())) for _ in range(N)]
-
-    min_val = [[INF]*N for _ in range(N)]   # 최소 금액을 저장할 배열
+# 연결 관계를 담을 리스트(1번부터 시작)
+lst = [[] for _ in range(V+1)]
 
 
-    def dijkstra(si,sj) :
-        q = []
-        min_val[si][sj] = arr[si][sj]             # 주의할 점! : 이 문제는 0,0도 가중치가 있음
-        heapq.heappush(q,(arr[si][sj],si,sj))     # 가중치, 시작점 좌표 # 가중치 값 주의
+for _ in range(E) :
+    u,v,w = map(int,input().split())        # u에서 v로 가는 가중치 w
+    lst[u].append((w,v))                    # 가중치, 연결 정점
 
 
-        while q :
-            loss, ci,cj = heapq.heappop(q)
+def dijkstra(start) :
+    q = []
+    heapq.heappush(q,(0,start))         # 시작지점의 가중치 0과 시작지점
+    min_val[start] = 0
+
+    while q :
+        cost, now  = heapq.heappop(q)
+
+        # 이미 있는 최단경로보다 크다면 패스
+        if cost > min_val[now] :
+            continue
+
+        # 현재 노드에서 다음 노드로 갈 때 필요한 임시 가중치 / 임시 지점
+        for next_cost, next in lst[now] :
+            go_cost = cost + next_cost       # pre 포인트로 갈 때 드는 총 가중치 = 현재 가중치 + (현재->다음 가중치)
+            if go_cost < min_val[next] :
+                min_val[next] = go_cost
+                heapq.heappush(q,(go_cost,next))
             
-            # 목적지 도달
-            if ci == N-1 and cj == N-1 : return
 
+# 다익스트라 호출
+dijkstra(start)
 
-            # 손실 금액이 최소 손실 금액보다 크다면 pass
-            if loss > min_val[ci][cj] : continue
-            for k in range(4) :
-                ni,nj = ci+di[k], cj+dj[k]
-                if 0<= ni < N and 0 <= nj < N :     # 범위 이내일 때
-                    go_loss = loss + arr[ni][nj]    # ni,nj에 가는데 드는 비용
-                    if go_loss < min_val[ni][nj] :  # 새로운 값이 더 작다면
-                        min_val[ni][nj] = go_loss   # 최소 비용 갱신
-                        heapq.heappush(q,(go_loss,ni,nj))
+# 출력
+for k in min_val[1:] :      # 0번은 필요없는 값
+    if k == INF :
+        print('INF')
+    else :
+        print(k)
 
-    dijkstra(0,0)
-    ans = min_val[N-1][N-1]
-    print(f'Problem {tc}: {ans}')
+    
 
             
             
