3439643aabf2302ca36906802b999cf5518bd370
unknown <jhc03032@naver.com>
2024-11-24 23:04:48 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -1,13 +1,8 @@
 import sys
 sys.stdin = open('C:/Users/jhc03/Desktop/포트폴리오/백준/test.txt')
 
-while True :
-    a,b = map(int,input().split())
-    
-    # 종료조건 
-    if a+b == 0 :
-        break
-    
-    print(a+b)
 
-    
+T = int(input())
+for _ in range(T) :
+    a,b = map(int,input().split())
+    print(a+b)
