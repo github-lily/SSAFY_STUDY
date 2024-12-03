087ebb5e61ea7de2cf709e819b3c2a9ca105cf1c
unknown <jhc03032@naver.com>
2024-12-04 01:07:44 +0900

    baekjoon_24444

--- 백준/test.py
+++ 백준/test.py
@@ -2,20 +2,43 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
-# 영수증의 총 금액
-X = int(input())
+from collections import deque
 
-# 구매한 물건의 종류 수
-N = int(input())
 
-# 물건들의 총합 계산
-total = 0
-for _ in range(N):
-    a, b = map(int, input().split())
-    total += a * b
+N,M,R = map(int,input().split())
+adj = [[] for _ in range(N+1)]
+visit = [0]*(N+1)
 
-# 결과 출력
-if total == X:
-    print("Yes")
-else:
-    print("No")
+# 인접리스트 받기
+for _ in range(M) :
+    u,v = map(int,input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+# 오름차순으로 정렬
+for edges in adj :
+    edges.sort()
+
+
+# 너비우선탐색
+def bfs(start) :
+    q = deque([start])
+    order = 1
+    visit[start] = order
+
+    while q :
+        c = q.popleft()
+        
+        for w in adj[c] :
+            if visit[w] == 0 :
+                order += 1
+                visit[w] = order
+                q.append(w)
+
+
+bfs(R)
+
+for i in range(1,N+1) :
+    print(visit[i])
+
+
