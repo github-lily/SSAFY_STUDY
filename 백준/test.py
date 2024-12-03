b39c0cd9454a495af2909208a4362cd7b7282d6d
unknown <jhc03032@naver.com>
2024-12-03 23:46:06 +0900

    1

--- 백준/test.py
+++ 백준/test.py
@@ -2,8 +2,20 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
-print("\\    /\\")
-print(" )  ( ')")
-print("(  /  )")
-print(" \\(__)|")
+# 영수증의 총 금액
+X = int(input())
 
+# 구매한 물건의 종류 수
+N = int(input())
+
+# 물건들의 총합 계산
+total = 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    total += a * b
+
+# 결과 출력
+if total == X:
+    print("Yes")
+else:
+    print("No")
