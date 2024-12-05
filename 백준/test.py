3f5016d02f349ecea30e2e42435670b6ff72c546
unknown <jhc03032@naver.com>
2024-12-06 01:27:55 +0900

    10816

--- 백준/test.py
+++ 백준/test.py
@@ -1,21 +1,19 @@
 import sys
 sys.stdin = open('백준/test.txt')
-# 시간초과
-# 아무래도 append, for문 2개,,
-# N값이 100만개까지라서 초과
+
+
+data = sys.stdin.read()
+
 
 N = int(input())
+card = list(map(int,input().split()))
+M = int(input())
+have = list(map(int,input().split()))
 
-# 원본 리스트
-X = list(map(int,input().split()))
+cnt = [0] * 500001
+for number in card :
+    cnt[number] += 1
 
-# 중복 제거 후 정렬(리스트로 반환됨)
-Y = sorted(set(X))     # [-10, -9, 2, 4]
 
-# enumerate : index, value를 반환
-num_dict = {value: idx for idx, value in enumerate(Y)}      
-
-# 결과 생성
-ans = [num_dict[number] for number in X]
-
+ans = [cnt[num] for num in have]
 print(*ans)
