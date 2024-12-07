9b7e0097ce3fb40459b13f5511d6975021871218
unknown <jhc03032@naver.com>
2024-12-07 17:51:24 +0900

    baekjoon_18258

--- 백준/test.py
+++ 백준/test.py
@@ -2,18 +2,51 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
-data = sys.stdin.read()
+import sys
+input = sys.stdin.readline
 
+from collections import deque
 
 N = int(input())
-card = list(map(int,input().split()))
-M = int(input())
-have = list(map(int,input().split()))
+q = deque()
+ans = 0
 
-cnt = [0] * 500001
-for number in card :
-    cnt[number] += 1
+for _ in range(N) :
+    command = input().split()
+
+    if len(command) > 1 :
+        x = int(command[1])
+        q.append(x)
+        
+    else :
+        if command[0] == 'pop' :
+            if q :
+                ans = q.popleft()
+            else :
+                ans = -1
+
+        elif command[0] == 'size' :
+            ans = len(q)
+
+        elif command[0] == 'empty' :
+            if q :
+                ans = 0
+            else :
+                ans = 1
+        
+        elif command[0] == 'front' :
+            if q :
+                ans = q[0]
+            else :
+                ans = -1
+
+        
+        elif command[0] == 'back' :
+            if q :
+                ans = q[-1]
+            else :
+                ans = -1
+
+        print(ans)
 
 
-ans = [cnt[num] for num in have]
-print(*ans)
