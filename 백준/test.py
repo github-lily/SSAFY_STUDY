78ab3270eeedb987649130e7d557fa2f36ff64c9
unknown <jhc03032@naver.com>
2024-12-13 23:51:16 +0900

    baekjoon_1629

--- 백준/test.py
+++ 백준/test.py
@@ -5,43 +5,5 @@ sys.stdin = open('백준/test.txt')
 import sys
 input = sys.stdin.readline
 
-M = int(input())
-S = set()
-
-# all_S = [x for x in range(1,21)]
-# 속도를 위해 set으로 만듦
-
-all_S = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
-
-
-for _ in range(M) :
-    request = input().split()
-
-    if request[0] == 'all' :
-        S = all_S
-
-    elif request[0] == 'empty' :
-        S = set()
-    
-    # 이 경우엔 무조건 request[1]이 존재함
-    else :
-        if request[0] == 'add' :
-            S.add(int(request[1]))
-
-        elif request[0] == 'remove' :
-            S.discard(int(request[1]))
-            # 값이 없어도 에러를 발생시키지 않음
-
-        elif request[0] == 'check' :
-            if int(request[1]) in S :
-                print(1)
-            else :
-                print(0)
-
-        elif request[0] == 'toggle' :
-            if int(request[1]) in S :
-                S.remove(int(request[1]))
-            else :
-                S.add(int(request[1]))
-
-
+a, b, c = map(int, input().split())
+print(pow(a, b, c))
