52ffbbe6fb0931ee8bb94468bbad5febf6131349
unknown <jhc03032@naver.com>
2024-12-15 15:49:25 +0900

    baekjoon_1259

--- 백준/test.py
+++ 백준/test.py
@@ -1,9 +1,24 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
+def check(number) :
 
-import sys
-input = sys.stdin.readline
 
-a, b, c = map(int, input().split())
-print(pow(a, b, c))
+    N = len(number)
+
+    for i in range(N//2) :
+        if number[i] != number[-i-1] :
+            print('no')
+            return
+            
+    print('yes')
+    return
+
+
+while True :
+    num = input()
+
+    if num == '0' :
+        break
+
+    check(num)
