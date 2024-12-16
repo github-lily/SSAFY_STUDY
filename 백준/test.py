34ecc6635e8d6b2f03f27da58a04bee4fc336332
unknown <jhc03032@naver.com>
2024-12-16 23:43:24 +0900

    baekjoon_12865

--- 백준/test.py
+++ 백준/test.py
@@ -1,24 +1,18 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-def check(number) :
 
+# 보텀업 방식(반복문 사용)
 
-    N = len(number)
+# 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
+dp = [0] * 6
 
-    for i in range(N//2) :
-        if number[i] != number[-i-1] :
-            print('no')
-            return
-            
-    print('yes')
-    return
+# dp 1과 2의 기본값 설정
+dp[1] = dp[2] = 1
+n = 5
 
+# 피보나치 함수를 반복문으로 구현
+for i in range(3, n+1) :
+    dp[i] = dp[i-1] + dp[i-2]
 
-while True :
-    num = input()
-
-    if num == '0' :
-        break
-
-    check(num)
+print(dp[i])
