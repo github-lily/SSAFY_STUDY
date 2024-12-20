e889b35725f082d95c44d1cc5508c14f73fca25f
unknown <jhc03032@naver.com>
2024-12-20 23:07:33 +0900

    baekjoon_1697

--- 백준/test.py
+++ 백준/test.py
@@ -1,10 +1,36 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-T = int(input())
-for _ in range(T) :
-    R,S = input().split()
 
-    for i in S :
-        print(i*int(R), end="")
-    print()
+from collections import deque
+
+
+
+def find(start,end) :
+
+    q = deque()
+    v = [0]*200001
+
+    q.append(start)
+    v[start] = 1
+
+    while q :
+        cn = q.popleft()
+
+        if cn == end :
+            return v[cn]-1
+        
+        for w in (2*cn, cn-1, cn+1) :
+            if 0<= w < 200000 :        # 범위 내
+                if v[w] ==0 :
+                    v[w] = v[cn] +1
+                    q.append(w)
+                    
+    return -1       # 디버깅용용
+
+
+N,K = map(int,input().split())
+
+
+ans = find(N,K)
+print(ans)
