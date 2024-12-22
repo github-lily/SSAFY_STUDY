d367ba5afe9b25cc733646775aad02a7e0b1bad3
unknown <jhc03032@naver.com>
2024-12-22 15:36:56 +0900

    baekjoon11659

--- 백준/test.py
+++ 백준/test.py
@@ -2,8 +2,20 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
+import sys
+input = sys.stdin.readline
+
 N,M = map(int,input().split())
-lst = list(map(int,input().split()))
+lst = [0] + list(map(int,input().split()))
+
+
+sum_lst = [0]*(N+1)
+sum_lst[1] = lst[1]
+
+for i in range(2,N+1) :
+    sum_lst[i] = sum_lst[i-1]+lst[i]
+
+
 for _ in range(M) :
     s,e = map(int,input().split())
-    print(sum(lst[s-1:e]))
+    print(sum_lst[e]-sum_lst[s-1])
