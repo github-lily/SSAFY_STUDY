f0bb820b5b27aa6dc00fe287ac35e4ad4c26bbf3
unknown <jhc03032@naver.com>
2024-12-26 16:50:23 +0900

    baekjoon_4153

--- 백준/test.py
+++ 백준/test.py
@@ -2,6 +2,26 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
-N = int(input())
-for i in range(1,N+1) :
-    print('*'*i)
+# while True :
+#     a,b,c = map(int,input().split())
+
+#     if a == b == c == 0 :
+#         break
+
+#     if a*a + b*b == c*c :
+#         print('right')
+#     else :
+#         print('wrong')
+
+
+while True :
+    lenght = sorted(map(int,input().split()))
+
+    if sum(lenght) == 0 :
+        break
+
+    if lenght[0]**2 + lenght[1]**2 == lenght[2]**2 :
+        print('right')
+    else :
+        print('wrong')
+
