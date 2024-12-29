9a3781d680148f887f265d57d7faad11b9e09aa5
unknown <jhc03032@naver.com>
2024-12-29 23:54:36 +0900

    11720

--- 백준/test.py
+++ 백준/test.py
@@ -1,27 +1,7 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-N = int(input())
+N =int(input())
+number = list(input())
 
-shirts = list(map(int,input().split()))
-T, P = map(int,input().split())
-
-# 구매해야하는 개수
-t = 0
-
-
-for size in shirts :
-    moc = size // T
-    remain = size % T
-
-    if remain == 0 :
-        t += moc
-    else :      # 나머지가 있다면 한세트 추가
-        t += (moc + 1)
-
-
-p_set = N // P
-p_each = N % P
-
-print(t)
-print(p_set, p_each)
+print(sum(map(int,number)))
