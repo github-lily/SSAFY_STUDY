1a81e27286b59f39edec17c384ed8d7684fd7172
unknown <jhc03032@naver.com>
2024-12-31 23:08:56 +0900

    1978

--- 백준/test.py
+++ 백준/test.py
@@ -1,7 +1,15 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-S = input()
-i = int(input())
+N = int(input())
+number = list(map(int,input().split()))
 
-print(S[i-1])
+ans = set()
+for num in number :    
+    if num == 3 or num == 5 or num == 7 :
+        ans.add(num)
+
+    elif not (num == 1 and num % 2 == 0 and num % 3 == 0  and num % 5 == 0  and num % 7 == 0)  :
+            ans.add(num)
+
+print(len(ans))
