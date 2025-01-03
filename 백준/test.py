c17e152c2f9a651339aa1fceea148fb81cf0128c
unknown <jhc03032@naver.com>
2025-01-03 23:40:08 +0900

    test

--- 백준/test.py
+++ 백준/test.py
@@ -1,15 +1,7 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-N = int(input())
-number = list(map(int,input().split()))
 
-ans = set()
-for num in number :    
-    if num == 3 or num == 5 or num == 7 :
-        ans.add(num)
-
-    elif not (num == 1 and num % 2 == 0 and num % 3 == 0  and num % 5 == 0  and num % 7 == 0)  :
-            ans.add(num)
-
-print(len(ans))
+N = reversed(sorted(list(input())))
+ans = ''.join(N)
+print(ans)
