339657ca3c968982a40958eb3882ff0b957f8232
unknown <jhc03032@naver.com>
2025-01-12 22:55:47 +0900

    2741

--- 백준/test.py
+++ 백준/test.py
@@ -2,20 +2,23 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
-s1 = input()
-s2 = input()
+N = int(input())
 
-n = len(s1)
-m = len(s2)
+i = 1
+while i <= N :
+    print(i)
+    i += 1
 
-dp = [0]*m
 
-for i in range(n) :
-    temp = 0
-    for j in range(m) :
-        if temp < dp[j] :
-            temp = dp[j]        # 지금까지의 최대값 저장
-        elif s1[i] == s2[j] :   # dp[j]의 값이 더 크다면 이미 계산된 글자이므로 elif(다시 계산할 필요 없음)
-            dp[j] = temp + 1    
 
-print(max(dp))
+# n = int(input())
+# arr = [list(map(int,input().split())) for _ in range(n)]
+
+
+# dp = [0] * n
+
+# floor = n
+
+# while floor > 0 :
+#     for i in range(n) :
+#         for j in range()
