0ffc5df5ee42cd54a7bfe43160f18f347af880af
unknown <jhc03032@naver.com>
2025-01-14 17:43:18 +0900

    1932

--- 백준/test.py
+++ 백준/test.py
@@ -1,21 +1,25 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-a = int(input())
-b = int(input())
-c = int(input())
 
-print(a+b-c)
-print(int(str(a)+str(b))-c)
-
-# n = int(input())
-# arr = [list(map(int,input().split())) for _ in range(n)]
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
 
 
-# dp = [0] * n
+dp = [[0] * n for _ in range(n)]
 
-# floor = n
 
-# while floor > 0 :
-#     for i in range(n) :
-#         for j in range()
+
+dp[0][0] = arr[0][0]
+
+
+for i in range(1,n) :
+    dp[i][0] = dp[i-1][0] + arr[i][0]       # 맨 왼쪽
+    dp[i][i] = dp[i-1][i-1] + arr[i][i]     # 맨 오른쪽
+    for j in range(1,i) :
+        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
+
+
+
+print(max(dp[n-1]))
+
