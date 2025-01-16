dc36f9d2d895f4da2a4450f44b6eada0733375a2
unknown <jhc03032@naver.com>
2025-01-16 22:25:42 +0900

    1991

--- 백준/test.py
+++ 백준/test.py
@@ -2,24 +2,32 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 
-n = int(input())
-arr = [list(map(int,input().split())) for _ in range(n)]
+N = int(input())
+arr = dict()
+
+for _ in range(N) :
+  p,l,r = input().split()
+  arr[p] = [l,r]
+
+pre_order = ''
+in_order=''
+post_order=''
 
 
-dp = [[0] * n for _ in range(n)]
+def order(T) :
+    global pre_order, in_order, post_order
+    
+    if T == '.' :
+        return
+    
+    pre_order += T
+    order(arr[T][0])
+    in_order += T
+    order(arr[T][1])
+    post_order += T
 
+order('A')
 
-
-dp[0][0] = arr[0][0]
-
-
-for i in range(1,n) :
-    dp[i][0] = dp[i-1][0] + arr[i][0]       # 맨 왼쪽
-    dp[i][i] = dp[i-1][i-1] + arr[i][i]     # 맨 오른쪽
-    for j in range(1,i) :
-        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j]
-
-
-
-print(max(dp[n-1]))
-
+print(pre_order)
+print(in_order)
+print(post_order)
