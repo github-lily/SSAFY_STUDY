59d9921de548a0d37116b7381ace153a8f40a553
hyunhee <jhc03032@naver.com>
2025-01-24 23:23:11 +0900

    9086

--- 백준/test.py
+++ 백준/test.py
@@ -1,15 +1,7 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-arr = [list(map(int,input().split())) for _ in range(9)]
-max_val = -1
-max_point = [0,0]
-
-for i in range(9) :
-  for j in range(9) :
-    if max_val < arr[i][j] :
-      max_val = arr[i][j]
-      max_point = [i+1,j+1]
-
-print(max_val)
-print(*max_point)
+T = int(input())
+for _ in range(T) :
+  text = list(input())
+  print(text[0]+text[-1])
