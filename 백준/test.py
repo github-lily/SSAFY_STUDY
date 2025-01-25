ee380d8e8b1bbfebb45d55bdf39f75eb4af5d494
hyunhee <jhc03032@naver.com>
2025-01-25 19:39:49 +0900

    15552

--- 백준/test.py
+++ 백준/test.py
@@ -1,7 +1,8 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-T = int(input())
-for _ in range(T) :
-  text = list(input())
-  print(text[0]+text[-1])
+N = int(sys.stdin.readline().strip())
+
+for _ in range(N) :
+  a,b = map(int,sys.stdin.readline().strip().split())
+  print(a+b)
