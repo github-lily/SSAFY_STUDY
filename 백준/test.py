3bd234b479b62eb8e5453ada24936ecdf1a4d514
hyunhee <jhc03032@naver.com>
2025-01-26 15:50:44 +0900

    1009

--- 백준/test.py
+++ 백준/test.py
@@ -3,6 +3,22 @@ sys.stdin = open('백준/test.txt')
 
 N = int(sys.stdin.readline().strip())
 
+
 for _ in range(N) :
-  a,b = map(int,sys.stdin.readline().strip().split())
-  print(a+b)
+  x,y = map(int,sys.stdin.readline().strip().split())
+
+  x = x%10
+  if x == 0 :
+    print(10)
+    continue
+
+
+  cycle = []
+  temp = x
+  while temp not in cycle :
+    cycle.append(temp)
+    temp = (temp * x) % 10
+
+  
+  idx = (y-1) % len(cycle)
+  print(cycle[idx])
