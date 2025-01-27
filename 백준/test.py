bdfbffec64afeeac1ec4b5a10dee0227984c6c6d
hyunhee <jhc03032@naver.com>
2025-01-28 00:14:17 +0900

    18108

--- 백준/test.py
+++ 백준/test.py
@@ -4,14 +4,4 @@ sys.stdin = open('백준/test.txt')
 N = int(sys.stdin.readline().strip())
 
 
-# 위쪽 삼각형 출력
-for i in range(1, N + 1):
-    spaces = ' ' * (N - i)
-    stars = '*' * (2 * i - 1)
-    print(spaces + stars)
-
-# 아래쪽 삼각형 출력
-for i in range(N - 1, 0, -1):
-    spaces = ' ' * (N - i)
-    stars = '*' * (2 * i - 1)
-    print(spaces + stars)
+print(N-543)
