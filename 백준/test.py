f21bc335278838da106a7fcb23e8d41f8a8ee8d8
hyunhee <jhc03032@naver.com>
2025-01-30 20:52:59 +0900

    test

--- 백준/test.py
+++ 백준/test.py
@@ -3,8 +3,13 @@ sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
 
-print("|\_/|")
-print("|q p|   /}")
-print("( 0 )\"\"\"\\")
-print("|\"^\"`    |")
-print("||_/=\\\\__|")
+
+while True :
+    a,b = map(int,input().split())
+    if a == b == 0 :
+        break
+
+    if a > b :
+        print('Yes')
+    else :
+        print('No')
