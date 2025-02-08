318089372b3a4e969cd1a9bc5a13772480625997
hyunhee <jhc03032@naver.com>
2025-02-08 16:27:49 +0900

    백준1032_명령프롬프트

--- 백준/test.py
+++ 백준/test.py
@@ -5,10 +5,27 @@ sys.stdin = open('백준/test.txt')
 
 N = int(input())
 
-num_lst = list(map(int,input().split()))
-m_val = max(num_lst)
-s_val = sum(num_lst)
+
+text = list(input())
+M = len(text)
+pre_text = text
+same_char = text
 
 
-ans = ((s_val / m_val) * 100) / N
+for _ in range(N-1) :
+    text = list(input())
+    for i in range(len(text)) :
+        if pre_text :
+            if pre_text[i] == text[i] :
+                if text[i] == same_char[i] :
+                    same_char[i] = text[i]
+                else :
+                    same_char[i] =  '?'
+            else :
+                same_char[i] =  '?'
+    
+    pre_text = text
+
+ans = ''.join(same_char)
 print(ans)
+
