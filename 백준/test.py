bbd4e9e7e851f08cdbc65e2f11f2d2873f5b633b
hyunhee <jhc03032@naver.com>
2025-02-11 23:53:29 +0900

    백준5622

--- 백준/test.py
+++ 백준/test.py
@@ -2,7 +2,13 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+word = input()
+time = 0
 
-import sys
-input = sys.stdin.read
-print(input())
+for char in word:
+    for i in range(len(dial)):
+        if char in dial[i]:
+            time += i + 3
+
+print(time)
