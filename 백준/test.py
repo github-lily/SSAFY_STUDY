c9af6ced6de07c56f28a41194be9b098acfe1986
hyunhee <jhc03032@naver.com>
2025-02-12 21:34:18 +0900

    백준3003번

--- 백준/test.py
+++ 백준/test.py
@@ -2,13 +2,14 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-word = input()
-time = 0
+# 체스 말의 기본 개수
+standard = [1, 1, 2, 2, 2, 8]
 
-for char in word:
-    for i in range(len(dial)):
-        if char in dial[i]:
-            time += i + 3
+# 입력값을 리스트로 변환
+input_pieces = list(map(int, input().split()))
 
-print(time)
+# 각 체스 말의 부족하거나 남은 개수 계산 후 출력
+result = [standard[i] - input_pieces[i] for i in range(6)]
+
+# 결과 출력
+print(*result)
