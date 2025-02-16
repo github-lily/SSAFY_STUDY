4a407eea52c93bb8637aaeade716609953eccf0e
hyunhee <jhc03032@naver.com>
2025-02-16 23:21:30 +0900

    백준_28278

--- 백준/test.py
+++ 백준/test.py
@@ -3,20 +3,36 @@ sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
 import sys
-import math
 
 # 입력 받기
 N = int(input())
-message_set = set()
-count = 0
+stack = []
 
 for _ in range(N) :
-    message = sys.stdin.readline().strip()
-    if message == "ENTER" :
-        message_set.clear()
-    elif message not in message_set :
-        message_set.add(message)
-        count += 1
+    cmd = list(map(int,sys.stdin.readline().split()))
 
+
+    if cmd[0] == 1 : # push
+        stack.append(cmd[1])
+
+    elif cmd[0] == 2 : # pop
+        if stack :
+            print(stack.pop())
+        else :
+            print(-1)
     
-print(count)
+    elif cmd[0] == 3 : # size
+        print(len(stack))
+
+    elif cmd[0] == 4 : # empty
+        if stack :
+            print(0)
+        else :
+            print(1)
+
+    elif cmd[0] == 5 : # top
+        if stack :
+            print(stack[-1])
+        else :
+            print(-1)
+
