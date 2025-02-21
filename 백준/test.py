fee9ed919f98ee58c32595463851b4c62540aec0
hyunhee <jhc03032@naver.com>
2025-02-21 18:43:50 +0900

    백준

--- 백준/test.py
+++ 백준/test.py
@@ -2,16 +2,16 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
-import sys
-import queue
+# import sys
+# import queue
 
-N = int(input())
-cards = [x for x in range(1,N+1)]
+# N = int(input())
+# cards = [x for x in range(1,N+1)]
 
 
-while len(cards) > 1 :
-    cards.pop(0)
-    a = cards.pop(0)
-    cards.append(a)
+# while len(cards) > 1 :
+#     cards.pop(0)
+#     a = cards.pop(0)
+#     cards.append(a)
 
-print(*cards)
+# print(*cards)
