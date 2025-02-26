56e1451cca06305634b5124ced9dc0bc5b2daa9a
hyunhee <jhc03032@naver.com>
2025-02-26 23:19:56 +0900

    백준2501

--- 백준/test.py
+++ 백준/test.py
@@ -3,47 +3,17 @@ sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
 
-from collections import deque
+N,K = map(int,input().split())
 
-N = int(input())
-q = deque()
-
-for _ in range(N) :
-    cmd = list(map(int,input().split()))
-
-    if cmd[0] == 1 :
-        q.appendleft(cmd[1])
-    
-    elif cmd[0] == 2 :
-        q.append(cmd[1])
-    
-    elif cmd[0] == 3 :
-        if q :
-            num = q.popleft()
-            print(num)
-        else :
-            print(-1)
-    
-    elif cmd[0] == 4 :
-        if q :
-            num = q.pop()
-            print(num)
-        else :
-            print(-1)
-    
-    elif cmd[0] == 5 :
-        len = len(q)
-        print(len)
-
-    elif cmd[0] == 6 :
-        if q :
-            print(0)
-        else :
-            print(1)
-    
-    elif cmd[0] == 7 :
-        if q :
-            q.
-        
+ans = []
+for i in range(1,N+1) :
+    rest = N % i
+    if rest == 0 :
+        if i not in ans :
+            ans.append(i)
+if len(ans) < K :
+    print(0)
+else :
+    print(sorted(ans)[K-1])
 
 
