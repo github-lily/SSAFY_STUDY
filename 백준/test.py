310cff9a11107a9a5841413a9a49330bbed6f26d
hyunhee <jhc03032@naver.com>
2025-02-27 10:25:08 +0900

    백준9663

--- 백준/test.py
+++ 백준/test.py
@@ -3,17 +3,35 @@ sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
 
-N,K = map(int,input().split())
 
-ans = []
-for i in range(1,N+1) :
-    rest = N % i
-    if rest == 0 :
-        if i not in ans :
-            ans.append(i)
-if len(ans) < K :
-    print(0)
-else :
-    print(sorted(ans)[K-1])
+def dfs(n) :
+    global ans
 
 
+    if n == N :
+        ans += 1
+        return
+    
+    for j in range(N) :
+        if v1[j] ==  v2[n+j] ==  v3[n-j] == 0 :
+            v1[j] = v2[n+j] = v3[n-j] = 1
+            dfs(n+1)
+            v1[j] = v2[n+j] = v3[n-j] = 0
+
+    
+
+N = int(input())
+ans = 0
+
+# 가로 확인
+v1 = [0] * N
+
+# 우상 대각선 확인
+v2 = [0] * (2*N) # 1 <= N <15 
+
+# 좌상 대각선 확인
+v3 = [0] *  (2*N)
+
+dfs(0)
+
+print(ans)
