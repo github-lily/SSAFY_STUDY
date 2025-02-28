fda6eaa0dfc0c7d32975c6d770ff3f16a8d47940
hyunhee <jhc03032@naver.com>
2025-02-28 22:58:19 +0900

    백준 2475

--- 백준/test.py
+++ 백준/test.py
@@ -3,35 +3,6 @@ sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
 
-
-def dfs(n) :
-    global ans
-
-
-    if n == N :
-        ans += 1
-        return
-    
-    for j in range(N) :
-        if v1[j] ==  v2[n+j] ==  v3[n-j] == 0 :
-            v1[j] = v2[n+j] = v3[n-j] = 1
-            dfs(n+1)
-            v1[j] = v2[n+j] = v3[n-j] = 0
-
-    
-
-N = int(input())
-ans = 0
-
-# 가로 확인
-v1 = [0] * N
-
-# 우상 대각선 확인
-v2 = [0] * (2*N) # 1 <= N <15 
-
-# 좌상 대각선 확인
-v3 = [0] *  (2*N)
-
-dfs(0)
-
-print(ans)
+nums = list(map(int, input().split()))  # 5개의 정수를 입력받아 리스트로 변환
+verification_number = sum(n**2 for n in nums) % 10  # 각 숫자의 제곱합을 구한 뒤 10으로 나눈 나머지 계산
+print(verification_number)  # 결과 출력
