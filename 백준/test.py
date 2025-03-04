4ff4f768cc8ae4a44062c742ebb17d70d561dd1e
hyunhee <jhc03032@naver.com>
2025-03-04 23:16:48 +0900

    백준11403

--- 백준/test.py
+++ 백준/test.py
@@ -2,5 +2,17 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
-a,b = map(int,input().split())
-print(a+b)
+N = int(input())
+adj = [list(map(int,input().split())) for _ in range(N) ]
+
+arr = [row[:] for row in adj]
+
+for k in range(N) :
+    for i in range(N) :
+        for j in range(N) :
+            if (arr[i][j] == 1) or (arr[i][k] == 1 and arr[k][j] == 1) :
+                arr[i][j] = 1
+
+
+for line in arr :
+    print(*line)
