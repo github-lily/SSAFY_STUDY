ccda4929195869678022d15bdab367a62aac9f10
hyunhee <jhc03032@naver.com>
2025-03-05 23:42:13 +0900

    백준_9465

--- 백준/test.py
+++ 백준/test.py
@@ -2,17 +2,22 @@ import sys
 sys.stdin = open('백준/test.txt')
 
 # N = int(sys.stdin.readline().strip())
-N = int(input())
-adj = [list(map(int,input().split())) for _ in range(N) ]
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(2)]
 
-arr = [row[:] for row in adj]
+    if N == 1:
+        print(max(arr[0][0], arr[1][0]))
+        continue
 
-for k in range(N) :
-    for i in range(N) :
-        for j in range(N) :
-            if (arr[i][j] == 1) or (arr[i][k] == 1 and arr[k][j] == 1) :
-                arr[i][j] = 1
+    dp = [[0] * N for _ in range(2)]
 
+    dp[0][0], dp[1][0] = arr[0][0], arr[1][0]
+    dp[0][1], dp[1][1] = arr[0][1] + dp[1][0], arr[1][1] + dp[0][0]
 
-for line in arr :
-    print(*line)
+    for j in range(2, N):
+        dp[0][j] = arr[0][j] + max(dp[1][j-1], dp[1][j-2])
+        dp[1][j] = arr[1][j] + max(dp[0][j-1], dp[0][j-2])
+
+    print(max(dp[0][N-1], dp[1][N-1]))
