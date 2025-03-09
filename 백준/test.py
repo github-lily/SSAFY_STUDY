e1da2028b52b175cbb089dfc7c755025a88ed2e8
hyunhee <jhc03032@naver.com>
2025-03-09 23:52:33 +0900

    백준_1085

--- 백준/test.py
+++ 백준/test.py
@@ -1,16 +1,7 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-# N = int(sys.stdin.readline().strip())
-N = int(input())
-F = int(input())
+x,y,w,h = map(int,input().split())
 
-# 마지막 두 자리를 00으로 변경
-N = (N // 100) * 100
-
-# F로 나누어떨어지는 가장 작은 수 찾기
-while N % F != 0:
-    N += 1
-
-# 마지막 두 자리만 출력
-print(str(N)[-2:])
+ans = min(x,y,w-x,h-y)
+print(ans)
