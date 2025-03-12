ffd13f4c7c34e37043cc9d5086b7a4e2b2339fd9
hyunhee <jhc03032@naver.com>
2025-03-12 21:35:15 +0900

    백준_13277

--- 백준/test.py
+++ 백준/test.py
@@ -1,18 +1,5 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-T = int(input())
-for _ in range(T) :
-    k = int(input())
-    n = int(input())
-
-    dp = [0] * n
-    for w in range(1,n+1) :
-        dp[w-1] = w
-
-    for _ in range(1, k+1) :
-        for i in range(1,n) :
-            dp[i] += dp[i-1]
-
-    
-    print(dp[n-1])
+a,b = map(int,input().split())
+ans = print(a*b)
