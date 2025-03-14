e287b21062e81bf17fa96482c93670343f602359
hyunhee <jhc03032@naver.com>
2025-03-14 22:28:53 +0900

    백준_10988

--- 백준/test.py
+++ 백준/test.py
@@ -1,5 +1,25 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-a,b = map(int,input().split())
-ans = print(a*b)
+
+def check() :
+    global word, N, half
+    if N % 2 == 0 :
+        for i in range(half) :
+            if word[half-i] != word[half+i-1] :
+                return 0
+
+    else :
+        for i in range(1,half+1) :
+            if word[half-i] != word[half+i] :
+                return 0
+            
+    return 1
+
+
+word = input()
+N = len(word)
+half = N // 2
+
+ans = check()
+print(ans)
