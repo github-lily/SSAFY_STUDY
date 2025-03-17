eff16ea1bc40523a1dc538a5c33096c6d313718b
hyunhee <jhc03032@naver.com>
2025-03-17 23:11:02 +0900

    백준 24262번

--- 백준/test.py
+++ 백준/test.py
@@ -1,28 +1,9 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-
-def check() :
-    for ans in range(n,N) :
-        ans_str = str(ans)
-        lenth = len(ans_str)
-        num = ans
-        # 자리수 더하기
-        for i in range(lenth) :
-            num += int(ans_str[i])
-        
-        # 일치하면 중단
-        if num == N :
-            return ans
-    
-    return 0
-        
-
+def MenOfPassion(A[], n) :
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
 
 N = int(input())
-M = len(str(N))
-n = int(N) - 9*M
 
-
-ans = check()
-print(ans)
