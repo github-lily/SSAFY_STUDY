01c8a219f6468aa49a74a0f22fae0509587e9848
hyunhee <jhc03032@naver.com>
2025-03-19 23:04:05 +0900

    백준_11286

--- 백준/test.py
+++ 백준/test.py
@@ -1,11 +1,21 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-jongsu = input().strip()
-doctor = input().strip()
+import heapq
 
-if len(jongsu) >= len(doctor):
-    print("go")
-else:
-    print("no")
+N = int(input())
+q = []
+for _ in range(N) :
+    x = int(input())
+
+    if x == 0 :
+        if q :
+            print(heapq.heappop(q)[1])
+
+        else :
+            print(0)
+    else :
+        heapq.heappush(q,(abs(x),x))
+    
+
 
