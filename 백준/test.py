5378e592d4a9cfe802a8c135f1c34e706bbde3d7
hyunhee <jhc03032@naver.com>
2025-03-20 23:37:21 +0900

    백준_11719번

--- 백준/test.py
+++ 백준/test.py
@@ -1,21 +1,4 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-import heapq
-
-N = int(input())
-q = []
-for _ in range(N) :
-    x = int(input())
-
-    if x == 0 :
-        if q :
-            print(heapq.heappop(q)[1])
-
-        else :
-            print(0)
-    else :
-        heapq.heappush(q,(abs(x),x))
-    
-
-
+print(sys.stdin.read(), end="")
