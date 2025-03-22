953f379fc91d357595aa0b9e7e2ccb618e69f05a
hyunhee <jhc03032@naver.com>
2025-03-22 23:58:38 +0900

    백준_1076

--- 백준/test.py
+++ 백준/test.py
@@ -1,7 +1,65 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-n = int(input())
-lst = list(map(int,input().split()))
+# def check() :
+#     global N, depth
 
-print(max(lst)* min(lst))
+#     num_lst = []
+#     while N // 2 > 1 :
+#         if N % 2 == 0 :     # 짝수라면
+#             for i in range(1, (N//2)+1) :
+#                 if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+#                     print(depth)
+#                     return
+#                 elif i+1 == user1 or i+1 == user2 :
+#                     num_lst.append(i+1)
+#                 else :
+#                     num_lst.append(i)
+
+#         else :              # 홀수라면
+#             for i in range(1, (N//2)+1) :
+#                 if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+#                     print(depth)
+#                     return
+#                 elif i+1 == user1 or i+1 == user2 :
+#                     num_lst.append(i+1)
+#                 else :
+#                     num_lst.append(i)
+            
+
+
+# # 리스트의 길이 N
+# N, user1, user2 = map(int,input().split())
+
+# depth = 1
+# lst = [x for x in range(1,N+1)]
+
+# while 
+#     for i in range(1, (N//2)+1) :
+#         if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+#             print(depth)
+#             return
+    
+
+# 리스트의 길이 N
+N, user1, user2 = map(int,input().split())
+
+
+lst = []
+depth = 1
+for i in range(1,N+1) :
+    # 답인 경우
+    if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+        print(depth)
+        break
+    
+    # i+1이 user1 또는 user2인 경우
+    elif i+1 == user1 :
+        lst.append(i+1)
+        user1 == lst[n]
+        
+    
+    else :
+        lst.append(i)
+    
+    depth += 1
