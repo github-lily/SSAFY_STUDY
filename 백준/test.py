6929e91e9ae5e6d5b89dddbf5742d3906ce43c83
hyunhee <jhc03032@naver.com>
2025-03-23 23:08:57 +0900

    백준_2577

--- 백준/test.py
+++ 백준/test.py
@@ -1,47 +1,47 @@
 import sys
 sys.stdin = open('백준/test.txt')
 
-# def check() :
-#     global N, depth
+def check() :
+    global N, depth
 
-#     num_lst = []
-#     while N // 2 > 1 :
-#         if N % 2 == 0 :     # 짝수라면
-#             for i in range(1, (N//2)+1) :
-#                 if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
-#                     print(depth)
-#                     return
-#                 elif i+1 == user1 or i+1 == user2 :
-#                     num_lst.append(i+1)
-#                 else :
-#                     num_lst.append(i)
+    num_lst = []
+    while N // 2 > 1 :
+        if N % 2 == 0 :     # 짝수라면
+            for i in range(1, (N//2)+1) :
+                if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+                    print(depth)
+                    return
+                elif i+1 == user1 or i+1 == user2 :
+                    num_lst.append(i+1)
+                else :
+                    num_lst.append(i)
 
-#         else :              # 홀수라면
-#             for i in range(1, (N//2)+1) :
-#                 if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
-#                     print(depth)
-#                     return
-#                 elif i+1 == user1 or i+1 == user2 :
-#                     num_lst.append(i+1)
-#                 else :
-#                     num_lst.append(i)
+        else :              # 홀수라면
+            for i in range(1, (N//2)+1) :
+                if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+                    print(depth)
+                    return
+                elif i+1 == user1 or i+1 == user2 :
+                    num_lst.append(i+1)
+                else :
+                    num_lst.append(i)
             
 
 
-# # 리스트의 길이 N
-# N, user1, user2 = map(int,input().split())
+# 리스트의 길이 N
+N, user1, user2 = map(int,input().split())
 
-# depth = 1
-# lst = [x for x in range(1,N+1)]
+depth = 1
+lst = [x for x in range(1,N+1)]
 
-# while 
-#     for i in range(1, (N//2)+1) :
-#         if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
-#             print(depth)
-#             return
+while 
+    for i in range(1, (N//2)+1) :
+        if (i == user1 and i+1 == user2) or (i == user2 and i+1 == user1) :
+            print(depth)
+            return
     
 
-# 리스트의 길이 N
+리스트의 길이 N
 N, user1, user2 = map(int,input().split())
 
 
@@ -63,3 +63,5 @@ for i in range(1,N+1) :
         lst.append(i)
     
     depth += 1
+
+
