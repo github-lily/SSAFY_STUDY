4d4748421eff9485692f7b9281db1aefae1fc997
hyunhee <jhc03032@naver.com>
2025-03-25 20:38:57 +0900

    백준_2750

--- 백준/test.py
+++ 백준/test.py
@@ -1,35 +1,43 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
-# 지수 찾는 함수
-def find(i,esp) :
+N = int(sys.stdin.readline().strip())
+arr = [int(sys.stdin.readline().strip()) for _ in range(N)]
+
+arr.sort()
+
+for num in arr :
+    print(num)
+
+# # 지수 찾는 함수
+# def find(i,esp) :
     
-    while True :
-        if i > 2**esp :
-            return esp-1
-        esp += 1
+#     while True :
+#         if i < 2**esp :
+#             return esp-1
+#         esp += 1
     
 
-def check(n,m) :
-    # 지수 찾기
-    x = find(n,0)
-    y = find(m,0)
+# def check(n,m) :
+#     # 지수 찾기
+#     x = find(n,0)
+#     y = find(m,0)
 
-    # 지수 그룹이 달라질때까지 계산
-    while True : 
-        # 지수 그룹이 다르면 큰 지수 그룹 
-        if x != y:
-            ans = max(x,y)
-            return ans
+#     # 지수 그룹이 달라질때까지 계산
+#     while True : 
+#         # 지수 그룹이 다르면 큰 지수 그룹 
+#         if x != y:
+#             ans = max(x,y)
+#             return ans
         
-        # 지수 그룹이 같으면 지수 그룹이 달라질때까지 계산 
-        else :
-            n = n - (2**(x-1))
-            m = m - (2**(x-1))
-            x = find(n,0)
-            y = find(m,0)
+#         # 지수 그룹이 같으면 지수 그룹이 달라질때까지 계산 
+#         else :
+#             n = n - (2**(x-1))
+#             m = m - (2**(x-1))
+#             x = find(n,0)
+#             y = find(m,0)
 
-length, n, m = map(int,input().split())
+# length, n, m = map(int,input().split())
 
-ans = check(n,m)
-print(ans)
+# ans = check(n,m)
+# print(ans)
