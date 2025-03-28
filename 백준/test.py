4e59954950990c73e01bd7989579c547baeb8233
hyunhee <jhc03032@naver.com>
2025-03-28 20:42:07 +0900

    백준_2587

--- 백준/test.py
+++ 백준/test.py
@@ -1,18 +1,10 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
-import sys
-from collections import Counter
+num_lst = sorted([int(input()) for _ in range(5)])
 
-word = sys.stdin.readline().strip().upper()  # 대문자로 변환
-counter = Counter(word)  # 알파벳 개수 세기
-
-max_count = max(counter.values())  # 가장 많이 등장한 횟수 찾기
-most_common = [char for char, count in counter.items() if count == max_count]  # 최다 빈도 문자 찾기
-
-# 최다 빈도 문자가 하나면 출력, 여러 개면 '?'
-print(most_common[0] if len(most_common) == 1 else '?')
-
+print(sum(num_lst)//5)
+print(num_lst[2])
 
 # # 지수 찾는 함수
 # def find(i,esp) :
