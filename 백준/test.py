efe89e62f979a52afa407bdf6c1a5f100a1f1c03
hyunhee <jhc03032@naver.com>
2025-04-07 23:48:03 +0900

    백준_1100

--- 백준/test.py
+++ 백준/test.py
@@ -1,16 +1,28 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
+chess = [list(input()) for _ in range(8)]
 
-a,b = map(int,input().split())
-A = set(map(int,input().split()))
-B = set(map(int,input().split()))
 
-AB = A-B
-BA = B-A
-ans = len(AB|BA)
+# 하얀칸
+# i가 짝수일 때 j도 짝수인 곳
+# i가 홀수일 때 j도 홀수인 곳
+ans = 0
+
+for i in range(8) :
+    for j in range(8) :
+        is_odd_j = j % 2
+        # 짝수일 때
+        if i % 2 == 0 :
+            if is_odd_j == 0 and chess[i][j] == "F" :
+                ans += 1
+        # 홀수일 때
+        else :
+            if is_odd_j == 1 and chess[i][j] == "F" :
+                ans += 1
+
 print(ans)
-
+             
 
 
 
