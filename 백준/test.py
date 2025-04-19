d1d0d8927b34320111792cd447e568ac9c93d3da
hyunhee <jhc03032@naver.com>
2025-04-19 10:36:00 +0900

    백준_11047

--- 백준/test.py
+++ 백준/test.py
@@ -1,33 +1,22 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
-N = int(input())
-ans = 0
+N, K = map(int,input().split())
+cnt = 0
+coins = []
 for _ in range(N) :
-    text = input()
-    n = len(text)
+    coins.append(int(input()))
 
-    # 한 글자인 경우
-    if n == 1 :
-        ans += 1
+# 그리디에 맞게 역순으로 출력
+coins = sorted(coins, reverse=True)
 
- 
-    # 한 글자 이상인 경우
-    else :
-        t_set = set(text[0])
-        flag = True
-        for i in range(1, len(text)) :
-            if text[i] in t_set :               # 중복된 수가 나왔을 때
-                if text[i-1] != text[i] :       # 이전값과 다르다면 중단
-                    flag = False
-                    break
-            else :
-                t_set.add(text[i])
-        if flag == True :
-            ans += 1
-            
+for coin in coins :
+    cnt += K // coin
+    K %= coin
 
-print(ans)
+print(cnt)
+
+
 
 # # 지수 찾는 함수
 # def find(i,esp) :
