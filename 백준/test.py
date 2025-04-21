698e102faecd862a9d534c0d7427fd7bfa791de1
hyunhee <jhc03032@naver.com>
2025-04-21 23:24:17 +0900

    백준_1026번

--- 백준/test.py
+++ 백준/test.py
@@ -1,24 +1,33 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
-n = int(input())
-player_list = []
-result = []
 
-for _ in range(n):
-    a = input()
-    player_list.append(a[0])
+N = int(input())
 
-first_names = set(player_list)
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
 
-for i in first_names:
-    if player_list.count(i) >= 5:
-        result.append(i)
+ans = 0
 
-if len(result) > 0:
-    print(''.join(sorted(result)))
-else:
-    print("PREDAJA")
+# B 인덱스와 함께 값 저장
+idx_B = []
+
+for i in range(N) :
+    idx_B.append((B[i],i))
+
+# B 오름차순 정렬
+idx_B.sort(key=lambda x:x[0])
+
+
+# A  내림차순 정렬(그리디)
+A.sort(reverse=True)
+
+
+
+for idx in range(N) :
+    ans += A[idx] * idx_B[idx][0]
+
+print(ans)
 
 # # 지수 찾는 함수
 # def find(i,esp) :
