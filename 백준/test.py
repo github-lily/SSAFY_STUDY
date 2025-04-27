2b7291226a7ee057f7d3c1c48c29a126ff0d9e97
hyunhee <jhc03032@naver.com>
2025-04-27 16:21:28 +0900

    백준_11725

--- 백준/test.py
+++ 백준/test.py
@@ -1,36 +1,37 @@
-import sys
+import sys 
 sys.stdin = open("백준/test.txt")
 
-order = list(map(int,input().split()))
+from collections import deque
 
-flag = 3        # 시작
+N = int(input())
 
-for i in range(1, 8) :
-    # 1씩 증가할 때
-    if order[i-1] + 1 == order[i] :
-        if flag == 0 :      # 이전에는 감소했다면 mixed
-            print("mixed")
-            break
-        else :
-            flag = 1        # 증가하면 flag = 1
-    # 1씩 감소할 때
-    elif order[i-1] - 1 == order[i] :
-        if flag == 1 :      # 이전에는 증가했다면 mixed
-            print("mixed")
-            break
-        else :
-            flag = 0        # 감소하면 flag = 0
-    # 1씩 증가하거나 감소하지 않을 때때
-    else :
-        print("mixed")
-        flag = 2
-        break      
 
-# 끝까지 순서대로 증가/감소를 확인하면 출력
-if flag == 1 :
-    print("ascending")
-elif flag == 0 :
-    print("descending")
+tree = [[] for _ in range(N+1)]
+p = [0] * (N+1)
+
+
+# 인접 노드 저장
+for _ in range(N-1) :
+    n1,n2 = map(int,input().split())
+    tree[n1].append(n2)
+    tree[n2].append(n1)
+
+
+# 트리 순회
+def bfs(start) :
+    q = deque([start])
+    while q :
+        current = q.popleft()
+        for next in tree[current] :
+            if p[next] == 0 :
+                p[next] = current
+                q.append(next)
+
+bfs(1)
+
+# 정답 출력
+for i in range(2,N+1) :
+    print(p[i]) 
 
 
 # # 지수 찾는 함수
