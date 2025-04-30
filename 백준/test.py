cc9b7ca8755cf7a413779849d2f5450f3582dd43
hyunhee <jhc03032@naver.com>
2025-04-30 20:16:43 +0900

    백준_10828

--- 백준/test.py
+++ 백준/test.py
@@ -1,26 +1,44 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
+
 import sys
 input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T) :
-    result = input().strip() 
-    N = len(result)
 
-    final_score = 0
-    score = 0
 
-    for mark in result :
-        if mark == 'O' :
-            score += 1
-            final_score += score
 
+N = int(input())
+stack = []
+for _ in range(N) :
+    cmd = input().strip().split()
+
+    # push 인 경우
+    if len(cmd) == 2 :
+        stack.append(cmd[1])
+    else :
+        text = cmd[0]
+        if text == "pop" :
+            if stack :
+                print(stack.pop())
+            else :
+                print(-1)    
+        
+        elif text == "size" :
+            print(len(stack))
+        
+        elif text == "empty" :
+            if stack :
+                print(0)
+            else :
+                print(1)
         else :
-            score = 0
+            if stack :
+                print(stack[-1])
+            else :
+                print(-1)
+            
 
-    print(final_score)
 
 
 # # 지수 찾는 함수
