f0718d5d128eb14aa29d107982ed145ca831fb4f
hyunhee <jhc03032@naver.com>
2025-05-01 16:00:44 +0900

    백준_1987

--- 백준/test.py
+++ 백준/test.py
@@ -5,38 +5,46 @@ sys.stdin = open("백준/test.txt")
 import sys
 input = sys.stdin.readline
 
+R,C = map(int,input().strip().split())
+arr = [list(input().strip()) for _ in range(R)]
+
+max_cnt = 1
+di, dj = [0,1,0,-1],[1,0,-1,0]
+
+
+# 알파벳 방문 배열
+dict = {'A':0,'B':0,'C':0,'D':0,'E':0,
+        'F':0,'G':0,'H':0,'I':0,'J':0,
+        'K':0,'L':0,'M':0,'N':0,'O':0,
+        'P':0,'Q':0,'R':0,'S':0,'T':0,
+        'U':0,'V':0,'W':0,'X':0,'Y':0,
+        'Z':0}
+
+
+def find(i,j,cnt) :
+    global max_cnt
+
+    if cnt > max_cnt :
+        max_cnt = cnt
+
+    for k in range(4) :
+        ni, nj = i+di[k], j+dj[k]
+        if 0 <= ni < R and 0 <= nj < C :        # 범위 내   
+            if dict[arr[ni][nj]] == 0 :
+                dict[arr[ni][nj]] = 1
+
+                # 다음으로 진행
+                find(ni,nj,cnt+1)
+
+                # 초기화
+                dict[arr[ni][nj]] = 0
+
+dict[arr[0][0]] = 1
+find(0,0,1)     # 처음 지점도 포함
+print(max_cnt)
 
 
 
-N = int(input())
-stack = []
-for _ in range(N) :
-    cmd = input().strip().split()
-
-    # push 인 경우
-    if len(cmd) == 2 :
-        stack.append(cmd[1])
-    else :
-        text = cmd[0]
-        if text == "pop" :
-            if stack :
-                print(stack.pop())
-            else :
-                print(-1)    
-        
-        elif text == "size" :
-            print(len(stack))
-        
-        elif text == "empty" :
-            if stack :
-                print(0)
-            else :
-                print(1)
-        else :
-            if stack :
-                print(stack[-1])
-            else :
-                print(-1)
             
 
 
