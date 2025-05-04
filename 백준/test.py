2b8b1fca9ef49ef69bfcc101496d82aff20a3b7c
hyunhee <jhc03032@naver.com>
2025-05-05 00:01:40 +0900

    백준_2869

--- 백준/test.py
+++ 백준/test.py
@@ -1,32 +1,19 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
-def find(H,W,N) :
-    cnt = 0
-    for j in range(W) :
-        for i in range(H) :
-            cnt += 1
-            if cnt == N :
-                room = (i+1)*100 + (j+1)
-                print(room)
-                return
-
-
-
 import sys
 input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T) :
-    h,w,n = map(int,input().split())
+A, B, V = map(int,input().split())
 
-    find(h,w,n)
+target = V-B
 
+one_day = A-B
 
+days = target // one_day
 
-# 높이는 상관 없음 무조건 엘베에서 가까운 방 선호
-# 거리가 같을 경우 낮은 방 선호
-
-
+if target % one_day != 0 :
+    days += 1
 
 
+print(days)
