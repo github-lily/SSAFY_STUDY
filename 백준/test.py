79a40e1b1aa4762b00f6511a230a8a08811befef
hyunhee <jhc03032@naver.com>
2025-05-08 23:04:48 +0900

    백준_5522

--- 백준/test.py
+++ 백준/test.py
@@ -1,42 +1,10 @@
-import sys
-sys.stdin = open("백준/test.txt")
+# import sys
+# sys.stdin = open("백준/test.txt")
 
-import sys
-input = sys.stdin.readline
-
-from collections import deque
-
-N = int(input())
-q = deque()
-
-for _ in range(N) :
-    cmd = list(input().strip().split())
-    req = cmd[0]
-    # print(req)
-    if req == 'push' :
-        q.append(cmd[1])
-    
-    elif req == 'size' :
-        print(len(q))
-    
-    else :
-        if q :
-            if req == 'pop' :
-                print(q.popleft())
-
-            elif req == 'empty' :
-                print(0)
-            elif req == 'front' :
-                print(q[0])
-            
-            elif req == 'back' :
-                print(q[-1])
-        else :
-            if req == 'empty' :
-                print(1)
-            else :
-                print(-1)
-            
-
-
-
+ans = 0
+while True :
+    try :
+        ans += int(input())
+    except EOFError :
+        break
+print(ans)
