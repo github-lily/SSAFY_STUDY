d1ec09153922be226ac869f39e5cb954dcec4cbe
unknown <jhc03032@naver.com>
2025-06-10 15:25:16 +0900

    백준_2822

--- 백준/test.py
+++ 백준/test.py
@@ -1,54 +1,25 @@
 import sys
 sys.stdin = open("백준/test.txt")
 
-
-
 import sys
 input = sys.stdin.readline
 
-from collections import deque
+lst = []
+for i in range(8) :
+    lst.append([int(input()),i])
 
-q = deque()
-N = int(input())
-for _ in range(N) :
-    cmd = input().split()
-    # print(len(cmd))
-    # print(cmd)
-    # print(cmd[0])
-    if cmd[0] == "push_front" :
-        q.appendleft(int(cmd[1]))
-    elif cmd[0] == "push_back" :
-        q.append(int(cmd[1]))
-    elif cmd[0] == "pop_front" :
-        if q :
-            print(q.popleft())
-        else :
-            print(-1)
-    elif cmd[0] == "pop_back" :
-        if q :
-            print(q.pop())
-        else :
-            print(-1)
-    elif cmd[0] == "size" :
-        print(len(q))
-    elif cmd[0] == "empty" :
-        if q :
-            print(0)
-        else :
-            print(1)
-    elif cmd[0] == "front" :
-        if q :
-            print(q[0])
-        else :
-            print(-1)
-    elif cmd[0] == "back" :
-        if q :
-            print(q[-1])
-        else :
-            print(-1)
-    else :
-        print("해당없음")
+lst = sorted(lst, reverse=True)
+scores = lst[:5]
 
+sum_val = 0
+ans_lst = []
+for score in scores :
+    sum_val += score[0]
+    ans_lst.append(score[1]+1)
+
+
+print(sum_val)
+print(*sorted(ans_lst))
 
 
 
