19fcdb6fb2d58f8b56772aefef5cd0b9251eb7d8
hyun <jhc03032@naver.com>
2025-07-26 23:44:47 +0900

    백준_11404

--- 백준/test.py
+++ 백준/test.py
@@ -6,24 +6,37 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-time_table = []
-for _ in range(N) :
-    s,e = map(int,input().split())
-    time_table.append([s,e])
+M = int(input())
 
-# 종료시간 빠른순 정렬 + 시작순서 빠른순 정렬(시작하자마자 끝나는 경우 고려)
-time_table.sort(key = lambda x : (x[1], x[0]))
-# print(arr)
+INF = int(1e9)
+# 1번부터 시작
+graph = [[INF] * (N+1) for _ in range(N+1)]
 
-k = 0
-cnt = 0
-for start, end in time_table :
-    if start >= k :
-        k = end
-        cnt += 1
+# 시작 == 도착 : 가중치 0
+for k in range(1,N+1) :
+    graph[k][k] = 0
+
+# 간선 정보 입력
+for _ in range(M) :
+    s,e,v = map(int,input().split())
+    # 노선이 여러개이므로 작은 값으로 저장
+    if graph[s][e] >= v :
+        graph[s][e] = v
+
+for k in range(1,N+1) :
+    for i in range(1,N+1) :
+        for j in range(1,N+1) :
+            graph[i][j] = min(graph[i][j],graph[i][k]+graph[k][j])
+
+for i in range(1,N+1) :
+    for j in range(1,N+1) :
+        if graph[i][j] == INF :
+            print("0", end=" ")
+        else :
+            print(graph[i][j], end=" ")
+    print()
 
 
-print(cnt)
 # import sys
 # input = sys.stdin.readline
 
